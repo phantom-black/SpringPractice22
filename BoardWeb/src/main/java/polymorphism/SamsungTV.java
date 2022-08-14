8c1c52feb346a7c76f9ba4704e7702566e5f5cde
phantom-black <black_phantom@naver.com>
2022-08-14 13:00:07 +0900

    220814 git 연동 test

---
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -0,0 +1,15 @@
+package polymorphism;
+
+public class SamsungTV implements TV {
+	public SamsungTV() {
+		System.out.println("==> SamsungTV 객체 생성");
+	}
+	
+	 public void powerOn() { System.out.println( "SamsungTv 전원 켬"); }
+	 
+	 public void powerOff() { System.out.println( "SamsungTv 전원 끔"); }
+	 
+	 public void volumeUp() { System.out.println( "SamsungTv 소리 올리기"); }
+	 
+	 public void volumeDown() { System.out.println( "SamsungTv 소리 내리기"); }
+}
---
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -0,0 +1,27 @@
+package polymorphism;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class TVUser {
+	public static void main(String[] args) {
+//		TV tv = new SamsungTV();
+//		tv.powerOn();
+//		tv.volumeDown();
+//		BeanFactory factory = new BeanFactory();
+//		TV tv = (TV)factory.getBean(args[0]);
+//		tv.powerOff();
+//		tv.volumeUp();
+		
+		//1. Spring 컨테이너를 구동한다
+		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		// 2. Spring 컨테이너로부터 필요한 객체 요청(Lookup)
+		TV tv = (TV)factory.getBean("tv");
+		tv.powerOff();
+		tv.volumeUp();
+		
+		// Spring 컨테이너 종료
+		factory.close();
+	}
+}
